f7b754f728d1ceb3a0c04464418c345942c3bf74
omergoshenis <omer.goshen@is.com>
2022-10-19 09:40:50 +0300

    add entities, dao, room

---
+++ app/src/main/java/com/example/tictactoeapplication/database/SingleGame.kt
@@ -0,0 +1,24 @@
+package com.example.tictactoeapplication.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "game_table")
+data class SingleGame(
+    @PrimaryKey(autoGenerate = true)
+    var gameId: Long = 0L,
+
+    @ColumnInfo(name = "game_state")
+    var gameState: Int = -1,
+
+    @ColumnInfo(name = "current_player")
+    var currentPlayer: Int = -1,
+
+    @ColumnInfo(name = "board_state")
+    var boardState: String = emptyBoard){
+    companion object{
+        var emptyBoard: String = "---------"
+    }
+}
+
